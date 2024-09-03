c77eafd63afb9b67566c604539229f49d3dd0675
Caiosouzadevv <caioss20.cs@gmail.com>
2024-09-03 06:02:04 -0300

    [FEAT]New button filter list

--- src/app/angular-material/angular-material.module.ts
+++ src/app/angular-material/angular-material.module.ts
@@ -6,6 +6,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
     imports:[
@@ -15,7 +16,8 @@ import { MatSelectModule } from '@angular/material/select';
         MatInputModule,
         MatDatepickerModule,
         MatNativeDateModule,
-         MatSelectModule
+         MatSelectModule,
+         MatButtonModule
 
     ],
     exports:[
@@ -25,7 +27,8 @@ import { MatSelectModule } from '@angular/material/select';
         MatInputModule,
         MatDatepickerModule,
         MatNativeDateModule,
-        MatSelectModule
+        MatSelectModule,
+        MatButtonModule
     ],
     
 })
