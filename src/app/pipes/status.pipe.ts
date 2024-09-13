1ad4c5d935114c082c71a64b6561f930df4f69c9
Caiosouzadevv <caioss20.cs@gmail.com>
2024-09-12 21:10:39 -0300

    [FEAT]New pipe status user list

---
+++ src/app/pipes/status.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'status'
+})
+export class StatusPipe implements PipeTransform {
+
+  transform(status: boolean): string {
+    
+   const invalid_status =  status === undefined || status === null;
+
+   if (invalid_status) {
+    return 'Status indisponível ou inválido';
+   }
+   return status ? 'Ativo' : 'Inativo';
+  }
+
+}
