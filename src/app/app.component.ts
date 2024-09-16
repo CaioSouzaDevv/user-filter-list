9ce21cb1a867a2e5525e3385ad25727ad071b9fd
Caiosouzadevv <caioss20.cs@gmail.com>
2024-09-16 05:49:12 -0300

    [FEAT]Simulating http data input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IUser } from './interfaces/user/user.interface';
 import { UsersList } from './data/user-list';
 
@@ -7,10 +7,25 @@ import { UsersList } from './data/user-list';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
 
-  userSelected: IUser = UsersList[0];
+export class AppComponent implements OnInit{
+
+  usersList: IUser[] = [];
+
+  userSelected: IUser = {} as IUser;
+  showUserDetails: boolean = false;
+
+  ngOnInit() {
+    console.log('onInit');
+    setTimeout(() => {
+      this.usersList = UsersList
+    }, 3000);
+  }
+
+
+
   onUserSelected(user: IUser) {
     this.userSelected = user;
+    this.showUserDetails = true;
   }
 }
