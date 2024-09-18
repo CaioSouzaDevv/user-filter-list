545139053d67165ea9193fca61697ea3f025f99f
Caiosouzadevv <caioss20.cs@gmail.com>
2024-09-18 05:59:53 -0300

    [FEAT]Creating name filter logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,14 +12,15 @@ import { iFilterOptions } from './interfaces/filter-options.interface';
 export class AppComponent implements OnInit{
 
   usersList: IUser[] = [];
-
+usersListFiltered: IUser[] = [];
   userSelected: IUser = {} as IUser;
   showUserDetails: boolean = false;
 
   ngOnInit() {
     console.log('onInit');
     setTimeout(() => {
-      this.usersList = UsersList
+      this.usersList = UsersList;
+      this.usersListFiltered = this.usersList;
     }, 1);
   }
 
@@ -32,5 +33,27 @@ export class AppComponent implements OnInit{
 
   onFIlter(filterOptions: iFilterOptions) {
     console.log(filterOptions);
+
+    this.usersListFiltered = this.filterUsersList(filterOptions, this.usersList);
   }
+
+  filterUsersList(filterOptions: iFilterOptions, UsersList: IUser[]): IUser[] {
+   let filteredList: IUser[] = [];
+
+   filteredList = this.filterUsersListByName(filterOptions.name, UsersList);
+   return filteredList;
+  }
+
+  filterUsersListByName(name: string | undefined, UsersList: IUser[]): IUser[] {
+const NAME_NOT_TYPPED = name === undefined;
+
+if(NAME_NOT_TYPPED) {
+  return this.usersList;
+}
+
+const filteredList = this.usersList.filter((user) => user.nome.toLowerCase().includes(name.toLowerCase()) );
+return filteredList;
+
+  }
+
 }
